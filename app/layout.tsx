b5b423e799c1ab90fed7222e94a48c90452f48a6
Philip Jurke <pjurke@icloud.com>
2025-04-11 16:21:14 +0200

    replace fonts + add to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
 import { JSX } from "react";
+import localFont from 'next/font/local'
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 
 import "./globals.css";
 
 import { Footer } from "@/components/Footer";
 
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: [ "100", "200", "300", "400", "500", "600", "700", "800", "900" ]
+const poppins = localFont({
+  src: [
+    { path: '../public/fonts/poppins/poppins-100.woff2', weight: '100', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-200.woff2', weight: '200', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-300.woff2', weight: '300', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-400.woff2', weight: '400', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-500.woff2', weight: '500', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-600.woff2', weight: '600', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-700.woff2', weight: '700', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-800.woff2', weight: '800', style: 'normal' },
+    { path: '../public/fonts/poppins/poppins-900.woff2', weight: '900', style: 'normal' },
+  ]
 });
 
 export const metadata: Metadata = {
