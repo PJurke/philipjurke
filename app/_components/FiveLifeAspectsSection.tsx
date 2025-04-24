0304f1a501a32c7a84fe9996a20e57cc22f1e789
Philip Jurke <pjurke@icloud.com>
2025-04-24 17:43:05 +0200

    add life aspect details

--- app/_components/FiveLifeAspectsSection.tsx
+++ app/_components/FiveLifeAspectsSection.tsx
@@ -3,9 +3,12 @@ export default function FiveLifeAspectsSection() {
     const lifeAspects: string[] = [ "Me", "Family", "Friends", "Work", "Society" ];
 
     return (
-        <section className="bg-slate-50 col-span-12 grid-cols-subgrid py-10 lg:py-20 text-center">
+        <section className="bg-slate-50 col-span-12 grid-cols-subgrid px-8 py-10 lg:py-20 text-center">
             <h2 className="font-light text-3xl">Five Life Aspects</h2>
-            <p className="mt-2 text-gray-500">My life is organized around five aspects:</p>
+            <p className="max-w-prose mt-2 mx-auto text-gray-500">
+                I organize my life around five life aspects.
+                My goal is to achieve long-term balance among them:
+            </p>
             <ul className="mt-8">
                 { lifeAspects.map((aspect) => (
                     <li className="p-4" key={aspect}>{aspect}</li>
