d37d72ca5ad740dcaa52f4813b2e615b57ee98c0
Philip Jurke <pjurke@icloud.com>
2025-04-24 18:05:57 +0200

    change h1 color

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
     return (
         <section className="bg-slate-50 col-span-12 flex items-center justify-center  min-h-[80vh]">
-            <h1 className="bg-clip-text bg-linear-to-tr from-teal-500 to-orange-500 font-light text-transparent text-5xl">Philip Jurke</h1>
+            <h1 className="bg-clip-text bg-linear-to-tr from-teal-500 to-amber-500 font-light text-transparent text-5xl">Philip Jurke</h1>
         </section>
     );
 }
