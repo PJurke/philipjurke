85c5c57730ec407a551b84240ff3be505214ffbf
Philip Jurke <pjurke@icloud.com>
2025-04-24 17:58:19 +0200

    outsource hero section, add linear color

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
+import HeroSection from "./_components/HeroSection";
 import FiveLifeAspectsSection from "./_components/FiveLifeAspectsSection";
 
 export default function Home() {
   return (
 
     <main className="grid grid-cols-12 gap-4">
-      <section className="bg-slate-50 col-span-12 flex items-center justify-center  min-h-[80vh]">
-        <h1 className="font-light text-5xl">Philip Jurke</h1>
-      </section>
-
+      <HeroSection />
       <FiveLifeAspectsSection />
     </main>
 
