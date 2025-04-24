252f269e733bf6200543246ecaa9c0077ad1ebde
Philip Jurke <pjurke@icloud.com>
2025-04-24 17:24:14 +0200

    use different words

--- app/_components/FiveLifeAspectsSection.tsx
+++ app/_components/FiveLifeAspectsSection.tsx
@@ -5,7 +5,7 @@ export default function FiveLifeAspectsSection() {
     return (
         <section className="bg-slate-50 col-span-12 grid-cols-subgrid py-10 lg:py-20 text-center">
             <h2 className="font-light text-3xl">Five Life Aspects</h2>
-            <p className="mt-2 text-gray-500">I orient my life around five aspects.</p>
+            <p className="mt-2 text-gray-500">My life is organized around five aspects:</p>
             <ul className="mt-8">
                 { lifeAspects.map((aspect) => (
                     <li className="p-4" key={aspect}>{aspect}</li>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 export function Footer(): JSX.Element {
     return (
         <footer className="bg-slate-50 mt-4 p-4 text-center">
-            <a className="hover:underline" href="mailto:webmaster@philipjurke.de">Write an e-mail to me</a>
+            <a className="hover:underline" href="mailto:webmaster@philipjurke.de">Send me an email.</a>
         </footer>
     );
 }
