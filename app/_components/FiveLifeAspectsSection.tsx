1818a2c3da2c28c1f28e785c9b2b0ed0656dad67
Philip Jurke <pjurke@icloud.com>
2025-06-23 20:25:02 +0200

    colors and spacing

--- app/_components/FiveLifeAspectsSection.tsx
+++ app/_components/FiveLifeAspectsSection.tsx
@@ -3,15 +3,15 @@ export default function FiveLifeAspectsSection() {
     const lifeAspects: string[] = [ "Me", "Family", "Friends", "Work", "Society" ];
 
     return (
-        <section className="bg-gray-50 col-span-12 grid-cols-subgrid px-8 py-10 lg:py-20 text-center">
-            <h2 className="font-light text-3xl">Five Life Aspects</h2>
-            <p className="max-w-prose mt-2 mx-auto text-gray-500">
+        <section className="col-span-12 grid-cols-subgrid px-8 py-8 lg:py-20 text-center">
+            <h2 className="font-extralight mb-2 text-4xl">Five Life Aspects</h2>
+            <p className="max-w-prose mb-4 mx-auto text-gray-400">
                 I organize my life around five life aspects.
                 My goal is to achieve long-term balance among them:
             </p>
-            <ul className="mt-8">
+            <ul>
                 { lifeAspects.map((aspect) => (
-                    <li className="p-4" key={aspect}>{aspect}</li>
+                    <li className="p-2" key={aspect}>{aspect}</li>
                 ))}
             </ul>
         </section>
--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
     return (
-        <section className="col-span-12 flex items-center justify-center min-h-[80vh]">
-            <h1 className="font-light text-gray-500 text-5xl">Philip Jurke</h1>
+        <section className="bg-yellow-50 col-span-12 flex items-center justify-center min-h-[80vh]">
+            <h1 className="font-extralight text-6xl text-yellow-500">Philip Jurke</h1>
         </section>
     );
 }
