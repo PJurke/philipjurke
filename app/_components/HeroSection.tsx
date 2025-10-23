0e4d47d9316522e21013a3e9e12c062ef7deee41
Philip Jurke <pjurke@icloud.com>
2025-10-23 20:05:54 +0200

    add sub title

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,7 +1,8 @@
 export default function HeroSection() {
     return (
-        <section className="bg-amber-50 col-span-12 flex items-center justify-center min-h-[80vh]">
+        <section className="bg-amber-50 col-span-12 flex flex-col items-center justify-center min-h-[80vh]">
             <h1 className="font-extralight text-6xl text-amber-500">Philip Jurke</h1>
+            <p className="mt-2 text-2xl text-gray-700">Agile Coach</p>
         </section>
     );
 }
